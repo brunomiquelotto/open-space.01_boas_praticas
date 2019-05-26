93f8422a8a2d74dedc597c02bfb9fa5ba6e4d0da
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2019-05-25 23:01:18 -0300

    Adicionar arquivos de projeto.

---
+++ LuxOpenSpace.Encontro1.Application/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿namespace LuxOpenSpace.Encontro1.Application
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+
+        public string Salvar()
+        {
+            var sucesso = true;
+            var mensagem = "";
+            if (this.Nome.Length == 0)
+            {
+                sucesso = false;
+                mensagem += "nome inválido";
+            }
+
+            if (this.Telefone.Length == 0)
+            {
+                sucesso = false;
+                mensagem += ";telefone inválido";
+            }
+
+            if (this.Cpf.Length == 0)
+            {
+                sucesso = false;
+                mensagem += ";cpf inválido";
+            }
+
+            if (sucesso)
+            {
+                //Salva
+                return "sucesso";
+            }
+            else
+            {
+                return mensagem;
+            }
+        }
+    }
+}
---
+++ LuxOpenSpace.Encontro1.Application/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LuxOpenSpace.Encontro1.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Pessoa();
+            var retorno = "";
+            a.Cpf = "123456789-12";
+            a.Nome = "José Fernando";
+            a.Telefone = "123456789";
+            retorno = a.Salvar();
+            Console.WriteLine(retorno);
+        }
+    }
+}
